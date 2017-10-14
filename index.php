7c627e2bc5b1e6853b859929338121e4ba5acaf8
belhard-user <ldarik2l@gmail.com>
2017-10-14 19:16:19 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP and Mysql</title>
+</head>
+<body>
+    <h1>Hello world!</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+    <h1>Hello world</h1>
+</body>
+</html>
