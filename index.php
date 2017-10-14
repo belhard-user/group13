5faf46a1a18c78a0a7c70004283c5f5e212da454
Helene <lenagritsuk@inbox.ru>
2017-10-14 20:24:55 +0300

    comment here 2

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <h1>
         <a href="/payment.php">Страница Оплаты</a>
     </h1>
-    <h1><a href="/forum.php">Форум</a></h1>
+    <h1>
+        <a href="/forum.php">
+            Форум
+        </a>
+    </h1>
     <h1>Hello world</h1>
     <h1>Hello world</h1>
 
